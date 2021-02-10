b0c8cbfb3f3cc75ad7ca14a69be49fb52a382397
Elyesa_Caglar <elyesa.caglar2002@gmail.com>
2021-02-10 10:49:33 +0100

    forms taak 1 af

---
+++ 3-Forms/taak01/invoer.php
@@ -0,0 +1,22 @@
++  <html>
+<body>
+
+<form action="mijngegevens.php" method="post">
+Naam <input type="text" name="Naam"><br>
+
+Achternaam: <input type="text" name="Achternaam"><br>
+
+klas: <input type="text" name="klas"><br>
+
+Leeftijd: <input type="text" name="leeftijd"><br>
+
+Adres: <input type="text" name="Adres"><br>
+
+plaatsnaam: <input type="text" name="plaatsnaam"><br>
+
+muziekband: <input type="text" name="muziekband"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
