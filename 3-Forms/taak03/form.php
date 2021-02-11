78b9cac287bf10eeb2402c6b1cdba6151eb4d7d3
Elyesa_Caglar <elyesa.caglar2002@gmail.com>
2021-02-11 19:08:46 +0100

    forms taak 3 af

---
+++ 3-Forms/taak03/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="mijnmuziek.php" method="get">
+Artist: <input type="text" name="Artist"><br>
+titel: <input type="text" name="titel"><br>
+jaar: <input type="text" name="jaar"><br>
+muzieksoort: <input type="text" name="muzieksoort"><br>
+<input type="submit">
+</form>
+
+
+<!-- http://localhost/jaar%202/php-advanced/3-Forms/taak03/mijnmuziek.php?Artist=michael+jackson&titel=beat+it&jaar=2002&muzieksoort=hip+hop -->
+
+
+</body>
+</html>
---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+naam van artist is: <?php echo $_GET["Artist"]; ?><br>
+de titel van het lied is: <?php echo $_GET["titel"]; ?><br>
+jaar: <?php echo $_GET["jaar"]; ?><br>
+muzieksoort: <?php echo $_GET["muzieksoort"]; ?><br>
+
+</body>
+</html>
